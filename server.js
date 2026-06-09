d3c13a27b26e6466d6efc53ec21cdc1fd0f48245
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2026-06-09 09:47:18 +0300

    removing security

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post('/login', async (req, res) => {
 // Other server setup and routes...
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://mihretunode:Q@methane.0fjzoxr.mongodb.net/signIn?retryWrites=true&w=majority")
+mongoose.connect("")
 .then(() => {
  console.log('MongoDB connected...')
  
